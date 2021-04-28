7530822544b3bdab1758986be1aaf8c3cd4e7d92
Emanuel Huber <emanuel.tesv@gmail.com>
2021-04-28 00:06:21 -0300

    Removed nltk_regex_tokenizer

--- tokenizer.py
+++ tokenizer.py
@@ -3,6 +3,7 @@ import argparse
 from nltk import tokenize
 
 from evaluate import read_test_data, evaluate_dataset
+from causal import TweetTokenizer
 
 
 def nltk_word_tokenizer(sentences: list) -> list:
@@ -13,27 +14,10 @@ def nltk_word_tokenizer(sentences: list) -> list:
 
     return pred_tokens
 
-def nltk_regex_tokenizer(sentences: list) -> list:
+def nltk_twitter_tokenizer(sentences: list) -> list:
     pred_tokens = []
 
-    tokenizer_regex = r'''(?ux)
-    # the order of the patterns is important!!
-    # more structured patterns come first
-    [a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)+|    # emails
-    (?:https?://)?\w{2,}(?:\.\w{2,})+(?:/\w+)*|                  # URLs
-    (?:[\#@]\w+)|                     # Hashtags and twitter user names
-    (?:[^\W\d_]\.)+|                  # one letter abbreviations, e.g. E.U.A.
-    (?:[DSds][Rr][Aa]?)\.|            # common abbreviations such as dr., sr., sra., dra.
-    (?:\B-)?\d+(?:[:.,]\d+)*(?:-?\w)*|
-        # numbers in format 999.999.999,999, possibly followed by hyphen and alphanumerics
-        # \B- avoids picks as F-14 as a negative number
-    \.{3,}|                           # ellipsis or sequences of dots
-    \w+|                              # alphanumerics
-    -+|                               # any sequence of dashes
-    \S                                # any non-space character
-    '''
-
-    tokenizer = tokenize.RegexpTokenizer(tokenizer_regex)
+    tokenizer = TweetTokenizer()
 
     for sentence in sentences:
         pred_tokens.append(tokenizer.tokenize(sentence))
@@ -43,14 +27,19 @@ def nltk_regex_tokenizer(sentences: list) -> list:
 def main():
 
     ids, sentences, true_tokens = read_test_data("data/tweets_stocks.csv", "data/dante_tweets1a50.conllu")
-
+    debug = True
     tokenizers = [
         ("nltk Word Tokenizer", nltk_word_tokenizer),
-        ("nltk regex Tokenizer", nltk_regex_tokenizer)
+        ("nltk Twitter Tokenizer", nltk_twitter_tokenizer)
     ]
 
     for name, tokenizer in tokenizers:
         pred_tokens = tokenizer(sentences)
+
+        if debug:
+            for pred, true in zip(pred_tokens, true_tokens):
+                print(f"{pred}\t\n{true}")
+                print("-"*20)
 
         precision, recall = evaluate_dataset(pred_tokens, true_tokens)
         
