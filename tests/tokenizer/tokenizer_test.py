9162822cf5221acc0265196bf6aad189a889e2ae
Emanuel Huber <emanuel.tesv@gmail.com>
2021-05-21 20:38:38 -0300

    Added tokenizer tests

--- tests/tokenizer/tokenizer_test.py
+++ tests/tokenizer/tokenizer_test.py
@@ -13,6 +13,15 @@ from dante_tokenizer import DanteTokenizer
     (":(", ":("),
     ("=)", "=)"),
     ("=(", "=("),
+    ("02/03/2020", "02/03/2020"),
+    ("20/21", "20/21"),
+    ("02.03.2020", "02.03.2020"),
+    ("20.21", "20.21"),
+    ("02-03-2020", "02-03-2020"),
+    ("20-21", "20-21"),
+    ("@user123", "@user123"),
+    ("#hashtag", "#hashtag"),
+    ("%k", "%k"),
 ])
 def test_tokenizer(raw_str: str, expected_str: str):
     dt = DanteTokenizer()
